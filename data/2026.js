2f7682aab1bd53070e37904d8aa3f9977522a70e
Hashem <hashemgamer85@gmail.com>
2026-09-12 12:25:58 +0330

    update object and add per2

--- data/2026.js
+++ data/2026.js
@@ -1,8 +1,17 @@
-per1 = {
-
-	"name":"Ali",
-	"last-name":"Khaki",
-	"phone-number":"+98933276128",
-	"email":"ali99khakiiii@gmail.com",
-	"address":"Iran, N-Khorasan, Bojnord, Emam streat"
+Employees = {
+	
+	per1 : {
+		"name":"Ali",
+		"last-name":"Khaki",
+		"phone-number":"+98933276128",
+		"email":"ali99khakiiii@gmail.com",
+		"address":"Iran, N-Khorasan, Bojnord, Emam streat"
+	},
+	per2 : {
+		"name":"reza",
+		"last-name":"Khaki",
+		"phone-number":"+989126729834",
+		"email":"rez2aaaaaai@gmail.com",
+		"address":"Iran, N-Khorasan, Bojnord, Khamene"
+	}
 }
